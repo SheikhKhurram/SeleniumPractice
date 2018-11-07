caef7b625040f2498bef7377cf0d71819738ca49
sheikhkhurram <sheik.kurram93@gmail.com>
2018-11-07 10:18:31 +0530

    added testNG retryLogic

---
+++ src/main/java/TestNGFeatures/ParametersTestNG.java
@@ -0,0 +1,28 @@
+package TestNGFeatures;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
+
+
+public class ParametersTestNG 
+{
+     
+	@Test
+	@Parameters({"url" , "userName"})
+	public void FBLogin(String url , String userName)
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumJar\\Chorme.exe\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get(url);
+		
+		driver.findElement(By.id("email")).sendKeys(userName);
+		
+		
+	}
+	
+	
+}
